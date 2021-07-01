4f0cfe86e6348b1d5f63b36dd88f18fb89b5d792
em-beth <85454614+em-beth@users.noreply.github.com>
2021-07-01 14:56:45 -0500

    Revert "thinking for search"
    This reverts commit c82728dea3e20c5faf334d899089f90b501085d1.

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
     let searchInput = document.querySelector('#search-input').value;
     console.log(event);
-    // alert("You are looking for " + searchInput)
     if(searchInput == 'name'){
         var className;
-        className = event.target.#searchform;
+      //  className = event.target.#searchform;
         console.log(className);
         const match = allPlants.find(element => element.name === className);
         console.log(match);
       }
+    // alert("You are looking for " + searchInput)
 });
-// if(searchInput == 'name'){
-//     var className;
-//     className = event.target.#searchform;
-//     console.log(className);
-//     const match = allPlants.find(element => element.name === className);
-//     console.log(match);
-//   }
 
 var dropdown = document.body.querySelector('.dropdown-content');
 dropdown.addEventListener('click', function(event){
