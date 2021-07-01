5a5fa0a1b7107e6f8c7804f55cc678f03624095c
Victoria Marsicovetere <vimarsicovetere@davidson.edu>
2021-07-01 14:42:48 -0400

    search bar js

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ let searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
     let searchInput = document.querySelector('#search-input').value;
-    console.log("submitted form", searchInput)
+    console.log(event);
+    alert("You are looking for " + searchInput)
 });
+
+    
+
