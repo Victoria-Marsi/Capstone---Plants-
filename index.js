c82728dea3e20c5faf334d899089f90b501085d1
Victoria Marsicovetere <vimarsicovetere@davidson.edu>
2021-07-01 15:52:31 -0400

    thinking for search

--- index.js
+++ index.js
@@ -3,43 +3,24 @@ searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
     let searchInput = document.querySelector('#search-input').value;
     console.log(event);
+    // alert("You are looking for " + searchInput)
     if(searchInput == 'name'){
         var className;
-      //  className = event.target.#searchform;
+        className = event.target.#searchform;
         console.log(className);
         const match = allPlants.find(element => element.name === className);
         console.log(match);
       }
-    // alert("You are looking for " + searchInput)
 });
+// if(searchInput == 'name'){
+//     var className;
+//     className = event.target.#searchform;
+//     console.log(className);
+//     const match = allPlants.find(element => element.name === className);
+//     console.log(match);
+//   }
 
-var dropdown = document.body.querySelector('.dropdown-content');
-dropdown.addEventListener('click', function(event){
-    console.log(event);
-    if(event.target.className === 'fruit'){
-        console.log(event.target.className);
-        for(let i = 0; i < allPlants.length; i++){
-            allPlants[i].show = false;
-            console.log(allPlants[i].show);
-            if(allPlants[i].subtype === 'Fruit'){
-                console.log("working");
-                allPlants[i].show = true;
-            }
-        }
-    }
-    if (event.target.className === 'herb'){
 
-    }
-    if(event.target.className === 'folliage'){
 
-    }
-    if(event.target.className === 'indoorFlower'){
+    
 
-    }
-    if(event.target.className === 'succulent'){
-
-    }
-    if(event.target.className === 'vegetable'){
-
-    }
-});
