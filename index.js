d587eddf894e90fde5b4cb5f333ec2d364252fc1
Victoria Marsicovetere <vimarsicovetere@davidson.edu>
2021-07-01 14:51:04 -0400

    search bar

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
     let searchInput = document.querySelector('#search-input').value;
     console.log(event);
-    alert("You are looking for " + searchInput)
+    if(searchInput == 'name'){
+        var className;
+        className = event.target.#searchform;
+        console.log(className);
+        const match = allPlants.find(element => element.name === className);
+        console.log(match);
+      }
+    // alert("You are looking for " + searchInput)
 });
+// if(searchInput == 'name'){
+//     var className;
+//     className = event.target.#searchform;
+//     console.log(className);
+//     const match = allPlants.find(element => element.name === className);
+//     console.log(match);
+//   }
+
+
 
     
 
